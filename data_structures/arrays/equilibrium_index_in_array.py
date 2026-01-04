2eaa74e87838c967730abcb31d5d0aa67d2320cc
vivekolkar <35103820+vivekolkar@users.noreply.github.com>
2026-01-04 17:54:08 +0530

    Update equilibrium_index_in_array.py

--- data_structures/arrays/equilibrium_index_in_array.py
+++ data_structures/arrays/equilibrium_index_in_array.py
@@ -11,7 +11,7 @@ an equilibrium index (if any) or -1 if no equilibrium index exists.
 The equilibrium index of an array is an index such that the sum of
 elements at lower indexes is equal to the sum of elements at higher indexes.
 
-
+Git
 
 Example Input:
 arr = [-7, 1, 5, 2, -4, 3, 0]
